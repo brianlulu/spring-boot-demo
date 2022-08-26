00841385deb3c2b80049b16b8ce4fbd513eee041
brianlulu <juiheng.lu@ryerson.ca>
2022-08-26 17:00:59 -0400

    Add emailCheck business logic and DELETE for api

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class StudentService {
@@ -19,7 +21,24 @@ public class StudentService {
 	}
 
 	public void addNewStudent(Student student) {
-		System.out.println(student);
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+		if (studentOptional.isPresent()) {
+			throw new IllegalStateException("Email is taken!");
+		}
+
+		studentRepository.save(student);
+	}
+
+	public void deleteStudent(Long studentId){
+		boolean exists = studentRepository.existsById(studentId);
+
+		if (!exists) {
+			throw new IllegalStateException(
+				"student with id " + studentId + " does not exist!"
+			);
+		}
+
+		studentRepository.deleteById(studentId);
 	}
 }
 
